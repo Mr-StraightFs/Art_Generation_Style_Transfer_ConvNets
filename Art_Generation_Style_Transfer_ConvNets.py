145b50c3af6e236855f9eaadf35341d2a0fe1ff2
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-23 16:08:10 +0100

    gram matrix function and style cost for a single layer

--- Art_Generation_Style_Transfer_ConvNets.py
+++ Art_Generation_Style_Transfer_ConvNets.py
@@ -46,3 +46,61 @@ def compute_content_cost(content_output, generated_output):
         tf.math.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled))))
 
     return J_content
+
+# gram_matrix
+# Using TensorFlow, let's implement a function that computes the Gram matrix of a matrix A.
+# The formula is: The gram matrix of A is  𝐺𝐴=𝐴𝐴𝑇
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+
+    GA = tf.linalg.matmul(A, tf.transpose(A))
+
+    return GA
+
+# Compute the style cost for a single layer.
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+
+    Returns:
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+
+    # Retrieve dimensions from a_G
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape the images from (n_H * n_W, n_C) to have them of shape (n_C, n_H * n_W)
+    a_S = tf.reshape(a_S, [n_H * n_W, n_C])
+    a_S = tf.transpose(a_S)
+    a_G = tf.reshape(a_G, [n_H * n_W, n_C])
+    a_G = tf.transpose(a_G)
+
+    # Computing gram_matrices for both images S and G
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the lose
+
+    factor = (.5 / (n_H * n_W * n_C)) ** 2
+    J_style_layer = factor * tf.reduce_sum(tf.square(tf.subtract(GS, GG)))
+
+    return J_style_layer
+
+for layer in vgg.layers:
+    print(layer.name)
+
+
+STYLE_LAYERS = [
+    ('block1_conv1', 0.2),
+    ('block2_conv1', 0.2),
+    ('block3_conv1', 0.2),
+    ('block4_conv1', 0.2),
+    ('block5_conv1', 0.2)]
