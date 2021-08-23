347f0e101b0501e1a5657d2e5c8c3ef38f3e7b41
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-23 13:07:02 +0100

    Computes the content cost

--- Art_Generation_Style_Transfer_ConvNets.py
+++ Art_Generation_Style_Transfer_ConvNets.py
@@ -9,3 +9,40 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework.ops import EagerTensor
 import pprint
+
+# Import the pretrained VGG Network from Keras Applications
+img_size = 400
+vgg = tf.keras.applications.VGG19(include_top=False,
+                                  input_shape=(img_size, img_size, 3),
+                                  weights='pretrained-model/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5')
+
+vgg.trainable = False
+
+# Neural Style Transfer (NST)
+
+def compute_content_cost(content_output, generated_output):
+    """
+    Computes the content cost
+
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+
+    Returns:
+    J_content -- scalar that you compute using equation 1 above.
+    """
+    a_C = content_output[-1]
+    a_G = generated_output[-1]
+
+    # Retrieve dimensions from a_G
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape a_C and a_G
+    a_C_unrolled = tf.reshape(a_C, [int(m), int(n_H * n_W), int(n_C)])
+    a_G_unrolled = tf.reshape(a_G, [int(m), int(n_H * n_W), int(n_C)])
+
+    # compute the cost with tensorflow
+    J_content = (1 / (4 * m * n_H * n_W * n_C)) * (
+        tf.math.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled))))
+
+    return J_content
